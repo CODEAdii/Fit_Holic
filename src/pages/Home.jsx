eab09ea499bab96194f9002d5c4b45d2834b269c
Adi <adityamauryamaurya144@gmail.com>
2024-06-28 01:33:04 +0545

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material'
+import React,{useState}from 'react'
+
+import Exercises from '../components/Exercise';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises'
+
+const Home = () => {
+  const [bodyPart,setBodyPart]=useState('all');
+  const [exercises ,setExercises]=useState([]);
+
+  return (
+    <Box>
+
+      <HeroBanner/>
+      <SearchExercises 
+        setExercises={setExercises}
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}/>
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart}/>
+
+    </Box>
+  )
+}
+
+export default Home
