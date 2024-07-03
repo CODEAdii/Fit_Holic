c15e98561a139e2fa83d66947024a7efd1f5edc5
Adi <adityamauryamaurya144@gmail.com>
2024-07-03 23:14:28 +0545

    Fifth Commit

--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -6,6 +6,13 @@ export const exerciseOptions = {
 		'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY
 	}
 };
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '5fb2b96242msha86bcfdfcd11e34p12b454jsnf4eecfb5817d',
+    },
+  };
 
 export const fetchData = async(url,options)=>{
     const response = await fetch(url,options);
